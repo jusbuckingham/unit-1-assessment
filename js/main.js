10794b8d3a10280368e045cbdd8a4e85720f1ac5
jusbuckingham <jus.buckingham@gmail.com>
2021-11-15 17:36:42 -0800

    adding the base elements

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 let input = 1;
 let count = 0;
+let input = document.querySelector('.input');
+let plusButton = document.querySelector('.plus');
+let minusButton = document.querySelector('.minus');
+
+
+updateInput();
+
+plusButton.addEventListener("click",function (e) {
+    count++;
+    updateInput();
+}) ;
+
+counterMinusElem.addEventListener("click",()=>{
+    count--;
+    updateInput();
+});
+
+function updateInput(){
+    counterDisplayElem.innerHTML = count;
+};
